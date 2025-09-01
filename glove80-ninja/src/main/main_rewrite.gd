b9ea6cfa724cd30a8c502070e28594f3b15a2b44
mathijs-bakker <mathijs_bakker@me.com>
2025-09-01 16:43:14 +0200

    TextRenderer, CursorRenderer and TypingController

--- glove80-ninja/src/main/main_rewrite.gd
+++ glove80-ninja/src/main/main_rewrite.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 
 func create_typing_controller() -> void:
 	var typing_scene = load("res://src/typing/typing.tscn")
+
 	# if not typing_scene:
 		# return null
 
 	var typing_controller = typing_scene.instantiate()
-	typing_controller.initialize("This is text")
+	# typing_controller.initialize("This is text")
 	add_child(typing_controller)
-	# return typing_controller

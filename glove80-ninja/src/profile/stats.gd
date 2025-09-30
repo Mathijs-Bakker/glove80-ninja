241b4d7fe6752d1eb637433e32b8c1250a90b1f1
mathijs-bakker <mathijs_bakker@me.com>
2025-09-30 22:21:42 +0200

    Show: Statistics All Time & Today

--- glove80-ninja/src/profile/stats.gd
+++ glove80-ninja/src/profile/stats.gd
@@ -6,6 +6,12 @@ extends Control
 @export var _all_time_avg_speed: Label
 @export var _all_time_top_accuracy: Label
 @export var _all_time_avg_accuracy: Label
+@export var _today_time: Label
+@export var _today_lessons: Label
+@export var _today_top_speed: Label
+@export var _today_avg_speed: Label
+@export var _today_top_accuracy: Label
+@export var _today_avg_accuracy: Label
 
 
 func _ready() -> void:
@@ -13,28 +19,52 @@ func _ready() -> void:
 
 
 func set_stats() -> void:
-	print("SET STATS")
+	var user_prf: Dictionary = UserService.get_profile()
 
-	var dict = UserService.get_profile()
-
-	if dict == null:
-		Log.Error("[profile][set_stats] Error fetching stats")
+	if user_prf == null:
+		Log.Error("[stats][set_stats] Error fetching stats")
 	else:
-		_all_time_time.text = format_duration(dict.get(UserService.Stats.TOTAL_TIME_TYPED))
-		_all_time_lessons.text = str(int(dict.get(UserService.Stats.TOTAL_SESSIONS)))
-		_all_time_top_speed.text = ("%swpm" % String.num(dict.get(UserService.Stats.BEST_WPM), 1))
-		_all_time_avg_speed.text = (
-			"%swpm" % String.num(dict.get(UserService.Stats.AVERAGE_WPM), 1)
-		)
-		_all_time_top_accuracy.text = (
-			"%swpm" % String.num(dict.get(UserService.Stats.BEST_ACCURACY), 1)
-		)
-		_all_time_avg_accuracy.text = (
-			"%swpm" % String.num(dict.get(UserService.Stats.AVERAGE_ACCURACY), 1)
-		)
+		var total_sessions_completed = user_prf.get(UserService.Stats.ALL_TIME_SESSIONS_COMPLETED)
 
-		for key_value in dict:  # shorthand
-			print(key_value, ":", dict[key_value])
+		if total_sessions_completed == 0:
+			return
+
+		_set_all_time_stats(user_prf)
+		_set_today_stats(user_prf)
+
+
+func _set_all_time_stats(p_user_prf: Dictionary) -> void:
+	_all_time_time.text = format_duration(p_user_prf.get(UserService.Stats.ALL_TIME_TIME_TYPED))
+	_all_time_lessons.text = str(int(p_user_prf.get(UserService.Stats.ALL_TIME_SESSIONS_COMPLETED)))
+	_all_time_top_speed.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_BEST_WPM), 1)
+	)
+	_all_time_avg_speed.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_AVERAGE_WPM), 1)
+	)
+	_all_time_top_accuracy.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_BEST_ACCURACY), 1)
+	)
+	_all_time_avg_accuracy.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_AVERAGE_ACCURACY), 1)
+	)
+
+
+func _set_today_stats(p_user_prf: Dictionary) -> void:
+	_today_time.text = format_duration(p_user_prf.get(UserService.Stats.TODAY_TIME_TYPED))
+	_today_lessons.text = str(int(p_user_prf.get(UserService.Stats.TODAY_SESSIONS_COMPLETED)))
+	_today_top_speed.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_BEST_WPM), 1)
+	)
+	_today_avg_speed.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_AVERAGE_WPM), 1)
+	)
+	_today_top_accuracy.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_BEST_ACCURACY), 1)
+	)
+	_today_avg_accuracy.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_AVERAGE_ACCURACY), 1)
+	)
 
 
 func format_duration(seconds: int) -> String:

eefe0c14fb559363ade187cf82f0fd4754d5a458
mathijs-bakker <mathijs_bakker@me.com>
2025-10-06 17:45:36 +0200

    Track users with id's and last online. If none exist create tracking and profile

--- glove80-ninja/src/autoload/user_model.gd
+++ glove80-ninja/src/autoload/user_model.gd
@@ -1,5 +1,9 @@
 extends Node
 
+const DEFAULT_USERS = {USERS.USER_IDS: [], USERS.LAST_LOGGED_IN: null}
+
+const USERS = {"USER_IDS": "user_ids", "LAST_LOGGED_IN": "last_logged_in"}
+
 # Default profile structure
 const DEFAULT_PROFILE = {
 	PROFILE.USERNAME: "New user",

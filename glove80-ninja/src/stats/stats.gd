29710f54c610f1258cec9bab508727a163c9cd4a
Mathijs Bakker <mathijs_bakker@me.com>
2025-10-07 09:03:27 +0200

    Revert "Refactor user constants"
    This reverts commit c8edba39f0502b7ce3638a4bbebf213bdcf906e4.

--- glove80-ninja/src/stats/stats.gd
+++ glove80-ninja/src/stats/stats.gd
@@ -24,7 +24,7 @@ func set_stats() -> void:
 	if user_prf == null:
 		Log.Error("[stats][set_stats] Error fetching stats")
 	else:
-		var total_sessions_completed = user_prf.get(User.STATS.ALL_TIME_SESSIONS_COMPLETED)
+		var total_sessions_completed = user_prf.get(UserService.Stats.ALL_TIME_SESSIONS_COMPLETED)
 
 		if total_sessions_completed == 0:
 			return
@@ -34,32 +34,36 @@ func set_stats() -> void:
 
 
 func _set_all_time_stats(p_user_prf: Dictionary) -> void:
-	_all_time_time.text = format_duration(p_user_prf.get(User.STATS.ALL_TIME_TIME_TYPED))
-	_all_time_lessons.text = str(int(p_user_prf.get(User.STATS.ALL_TIME_SESSIONS_COMPLETED)))
+	_all_time_time.text = format_duration(p_user_prf.get(UserService.Stats.ALL_TIME_TIME_TYPED))
+	_all_time_lessons.text = str(int(p_user_prf.get(UserService.Stats.ALL_TIME_SESSIONS_COMPLETED)))
 	_all_time_top_speed.text = (
-		"%swpm" % String.num(p_user_prf.get(User.STATS.ALL_TIME_BEST_WPM), 1)
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_BEST_WPM), 1)
 	)
 	_all_time_avg_speed.text = (
-		"%swpm" % String.num(p_user_prf.get(User.STATS.ALL_TIME_AVERAGE_WPM), 1)
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_AVERAGE_WPM), 1)
 	)
 	_all_time_top_accuracy.text = (
-		"%swpm" % String.num(p_user_prf.get(User.STATS.ALL_TIME_BEST_ACCURACY), 1)
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_BEST_ACCURACY), 1)
 	)
 	_all_time_avg_accuracy.text = (
-		"%swpm" % String.num(p_user_prf.get(User.STATS.ALL_TIME_AVERAGE_ACCURACY), 1)
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.ALL_TIME_AVERAGE_ACCURACY), 1)
 	)
 
 
 func _set_today_stats(p_user_prf: Dictionary) -> void:
-	_today_time.text = format_duration(p_user_prf.get(User.STATS.TODAY_TIME_TYPED))
-	_today_lessons.text = str(int(p_user_prf.get(User.STATS.TODAY_SESSIONS_COMPLETED)))
-	_today_top_speed.text = ("%swpm" % String.num(p_user_prf.get(User.STATS.TODAY_BEST_WPM), 1))
-	_today_avg_speed.text = ("%swpm" % String.num(p_user_prf.get(User.STATS.TODAY_AVERAGE_WPM), 1))
+	_today_time.text = format_duration(p_user_prf.get(UserService.Stats.TODAY_TIME_TYPED))
+	_today_lessons.text = str(int(p_user_prf.get(UserService.Stats.TODAY_SESSIONS_COMPLETED)))
+	_today_top_speed.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_BEST_WPM), 1)
+	)
+	_today_avg_speed.text = (
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_AVERAGE_WPM), 1)
+	)
 	_today_top_accuracy.text = (
-		"%swpm" % String.num(p_user_prf.get(User.STATS.TODAY_BEST_ACCURACY), 1)
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_BEST_ACCURACY), 1)
 	)
 	_today_avg_accuracy.text = (
-		"%swpm" % String.num(p_user_prf.get(User.STATS.TODAY_AVERAGE_ACCURACY), 1)
+		"%swpm" % String.num(p_user_prf.get(UserService.Stats.TODAY_AVERAGE_ACCURACY), 1)
 	)
 
 

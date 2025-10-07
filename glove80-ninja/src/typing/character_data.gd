ef901b37aa1ce8d46bb88834b285a1cb6aea63ce
Mathijs Bakker <mathijs_bakker@me.com>
2025-10-07 09:03:27 +0200

    Revert "Wip: KeyInputHandler moved to scene"
    This reverts commit 1c6514df97e16b68f73ecd0ca729ec6520fac175.

--- glove80-ninja/src/typing/character_data.gd
+++ glove80-ninja/src/typing/character_data.gd
@@ -6,12 +6,12 @@ var _value: String
 var _position: Vector2
 var _label_size: Vector2
 var _line_height: float
-var is_correct: bool
+var is_wrong: bool
 
 
-func _init(p_value: String, p_pos: Vector2, p_size: Vector2, p_line_height: float, p_is_correct: bool = false) -> void:
+func _init(p_value: String, p_pos: Vector2, p_size: Vector2, p_line_height: float, p_is_wrong: bool = false) -> void:
     _value = p_value
     _position = p_pos
     _label_size = p_size 
     _line_height = p_line_height
-    is_correct = p_is_correct
+    is_wrong = p_is_wrong
